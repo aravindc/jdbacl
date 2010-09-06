1502ef370b5efa1816c47079fb07f6cb0c9e8fbd
vbergmann <vbergmann@ae0afc29-7cfe-489a-95cf-4bb878f6dcf4>
2010-09-06 10:00:51 +0000

    new class SQLUtil
    git-svn-id: https://svn.code.sf.net/p/jdbacl/code/trunk@31 ae0afc29-7cfe-489a-95cf-4bb878f6dcf4

---
+++ src/main/java/org/databene/jdbacl/SQLUtil.java
@@ -0,0 +1,40 @@
+/*
+ * (c) Copyright 2010 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.jdbacl;
+
+/**
+ * TODO Document class.<br/><br/>
+ * Created: 01.09.2010 09:38:46
+ * @since TODO version
+ * @author Volker Bergmann
+ */
+public class SQLUtil {
+
+	public static String renderValue(Object value) {
+		return (value instanceof String ? "'" + value.toString() + "'" : String.valueOf(value));
+	}
+	
+	public static String substituteMarkers(String sql, String marker, Object substitution) {
+		return sql.replace(marker, renderValue(substitution));
+    }
+
+}
