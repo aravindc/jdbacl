90006654fd1a98fcc30ed6e7726155b6705e1a79
vbergmann <vbergmann@ae0afc29-7cfe-489a-95cf-4bb878f6dcf4>
2010-10-31 10:50:31 +0000

    Renamed DBImporter to DBMetaDataImporter
    git-svn-id: https://svn.code.sf.net/p/jdbacl/code/trunk@55 ae0afc29-7cfe-489a-95cf-4bb878f6dcf4

--- src/main/java/org/databene/jdbacl/model/DBMetaDataImporter.java
+++ src/main/java/org/databene/jdbacl/model/DBMetaDataImporter.java
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2006-2009 by Volker Bergmann. All rights reserved.
+ * (c) Copyright 2006-2010 by Volker Bergmann. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, is permitted under the terms of the
@@ -33,6 +33,6 @@ import org.databene.commons.ImportFailedException;
  * Created: 06.01.2007 19:17:27
  * @author Volker Bergmann
  */
-public interface DBImporter {
-    Database importDatabase() throws ImportFailedException ;
+public interface DBMetaDataImporter {
+    Database importDatabase() throws ImportFailedException;
 }
