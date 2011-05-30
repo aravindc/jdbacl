d76b35aa100fb41eb04a336c16a2dc0047c2a47c
vbergmann <vbergmann@ae0afc29-7cfe-489a-95cf-4bb878f6dcf4>
2011-05-30 07:03:19 +0000

    Javadoc
    git-svn-id: https://svn.code.sf.net/p/jdbacl/code/trunk@203 ae0afc29-7cfe-489a-95cf-4bb878f6dcf4

--- src/main/java/org/databene/jdbacl/model/DBObject.java
+++ src/main/java/org/databene/jdbacl/model/DBObject.java
@@ -33,8 +33,17 @@ import org.databene.commons.Named;
  * @author Volker Bergmann
  */
 public interface DBObject extends Named, Serializable {
+	
+	/** @return the type of the DBObject as used in DDL in lower case letters. */
     public String getObjectType();
+    
+    /** @return documentation of the DBObject if available, otherwise null. */
     public String getDoc();
+    
+    /** @return the owner of the DBObject instance or null if no owner has been set. */
     public CompositeDBObject<?> getOwner();
+    
+    /** sets the owner of the DBObject instance. */
 	public void setOwner(CompositeDBObject<?> owner);
+	
 }
