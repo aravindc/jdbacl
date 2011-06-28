2fd2b747a472ca4081da61ca5acaff427e22c9c2
vbergmann <vbergmann@ae0afc29-7cfe-489a-95cf-4bb878f6dcf4>
2011-06-28 16:56:10 +0000

    Fixed task version
    git-svn-id: https://svn.code.sf.net/p/jdbacl/code/trunk@295 ae0afc29-7cfe-489a-95cf-4bb878f6dcf4

--- src/main/java/org/databene/jdbacl/identity/SimpleTranscoder.java
+++ src/main/java/org/databene/jdbacl/identity/SimpleTranscoder.java
@@ -67,7 +67,7 @@ public class SimpleTranscoder {
 		return row;
     }
 
-	/* v0.7 TODO use this for merging
+	/* TODO v0.7 use this for merging
 	@Override
     public void merge(Connection source, Connection target, int pageSize,
 			KeyMapper mapper, Context context) {
