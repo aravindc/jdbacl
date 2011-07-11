ff58aaf311007fc1129f429780e0b328546b36f2
vbergmann <vbergmann@ae0afc29-7cfe-489a-95cf-4bb878f6dcf4>
2011-07-11 08:42:29 +0000

    Defined test for MySQL dialect
    git-svn-id: https://svn.code.sf.net/p/jdbacl/code/trunk@329 ae0afc29-7cfe-489a-95cf-4bb878f6dcf4

---
+++ src/test/java/org/databene/jdbacl/dialect/MySQLDialectTest.java
@@ -0,0 +1,81 @@
+/*
+ * (c) Copyright 2011 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.jdbacl.dialect;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests the {@link MySQLDialect}.<br/><br/>
+ * Created: 10.07.2011 10:06:28
+ * @since 0.6.10
+ * @author Volker Bergmann
+ */
+public class MySQLDialectTest extends DatabaseDialectTest<MySQLDialect> {
+
+	public MySQLDialectTest() {
+	    super(new MySQLDialect());
+    }
+
+	@Test
+	public void testSequenceSupported() {
+		assertFalse(dialect.isSequenceSupported());
+	}
+	
+	@Test
+	public void testFormatDate() {
+		assertEquals("'1971-02-03'", dialect.formatValue(DATETIME_19710203131415));
+	}
+	
+	@Test
+	public void testFormatTime() {
+		assertEquals("'13:14:15'", dialect.formatValue(TIME_131415));
+	}
+	
+	@Test
+	public void testFormatTimestamp() {
+		assertEquals("'1971-02-03 13:14:15.123456789'", 
+				dialect.formatValue(TIMESTAMP_19710203131415123456789));
+	}
+	
+	@Test
+	public void testIsDeterministicPKName() {
+		assertTrue(dialect.isDeterministicPKName("USER_PK"));
+	}
+	
+	@Test
+	public void testIsDeterministicUKName() {
+		assertTrue(dialect.isDeterministicUKName("USER_NAME_UK"));
+	}
+	
+	@Test
+	public void testIsDeterministicFKName() {
+		assertTrue(dialect.isDeterministicFKName("USER_ROLE_FK"));
+	}
+	
+	@Test
+	public void testIsDeterministicIndexName() {
+		assertTrue(dialect.isDeterministicIndexName("USER_NAME_IDX"));
+	}
+	
+}
