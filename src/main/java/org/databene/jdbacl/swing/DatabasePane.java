753a099f922be24d4a42685fce4c9f578de10df6
vbergmann <vbergmann@ae0afc29-7cfe-489a-95cf-4bb878f6dcf4>
2011-11-11 10:54:42 +0000

    Implemented minimalistic GUI
    git-svn-id: https://svn.code.sf.net/p/jdbacl/code/trunk@446 ae0afc29-7cfe-489a-95cf-4bb878f6dcf4

---
+++ src/main/java/org/databene/jdbacl/swing/DatabasePane.java
@@ -0,0 +1,110 @@
+/*
+ * (c) Copyright 2011 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.jdbacl.swing;
+
+import java.awt.BorderLayout;
+import java.io.IOException;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.SwingUtilities;
+import javax.swing.tree.TreeModel;
+
+import org.databene.commons.ConnectFailedException;
+import org.databene.commons.ImportFailedException;
+import org.databene.gui.swing.SwingTreeModelAdapter;
+import org.databene.jdbacl.DatabaseTreeModel;
+import org.databene.jdbacl.model.DBMetaDataImporter;
+import org.databene.jdbacl.model.DBObject;
+import org.databene.jdbacl.model.Database;
+import org.databene.jdbacl.model.cache.CachingDBImporter;
+import org.databene.jdbacl.model.jdbc.JDBCDBImporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * {@link JPanel} which displays database information and the hierarchy tree.<br/><br/>
+ * Created: 07.11.2011 16:46:33
+ * @since 0.7.0
+ * @author Volker Bergmann
+ */
+@SuppressWarnings("serial")
+public class DatabasePane extends JPanel {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(DatabasePane.class);
+	
+	private JScrollPane scrollPane;
+	private DatabaseTree tree;
+	private DBMetaDataImporter importer;
+	private TextFieldValueProvider exclusionPatternProvider;
+
+	public DatabasePane(TextFieldValueProvider exclusionPatternProvider) {
+		super(new BorderLayout());
+		this.exclusionPatternProvider = exclusionPatternProvider;
+		this.scrollPane = new JScrollPane();
+		add(scrollPane, BorderLayout.CENTER);
+	}
+	
+	public void setEnvironment(String environment) throws ConnectFailedException, ImportFailedException {
+		if (importer != null) {
+			try {
+				importer.close();
+			} catch (IOException e) {
+				LOGGER.error("Error closing " + getClass().getName(), e);
+			}
+			if (tree != null)
+				scrollPane.remove(tree);
+		}
+		new Thread(new Importer(environment)).start();
+	}
+
+	class Importer implements Runnable {
+		String environment;
+		public Importer(String environment) {
+			this.environment = environment;
+		}
+		public void run() {
+			try {
+				JDBCDBImporter parser = new JDBCDBImporter(environment);
+				parser.setExcludeTables(exclusionPatternProvider.getValue());
+				parser.setImportingChecks(true);
+				parser.setImportingIndexes(true);
+				parser.setImportingSequences(true);
+				parser.setImportingTriggers(true);
+				parser.setImportingPackages(true);
+				parser.setImportingUKs(true);
+				CachingDBImporter importer = new CachingDBImporter(parser, environment);
+				Database database = importer.importDatabase();
+				DatabasePane.this.importer = importer;
+				final TreeModel model = new SwingTreeModelAdapter<DBObject>(new DatabaseTreeModel(database));
+				SwingUtilities.invokeLater(new Runnable() {
+					public void run() {
+						tree = new DatabaseTree(model);
+						scrollPane.setViewportView(tree);
+					}
+				});
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+}
---
+++ src/main/java/org/databene/jdbacl/swing/JdbaclGUI.java
@@ -0,0 +1,172 @@
+/*
+ * (c) Copyright 2011 by Volker Bergmann. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, is permitted under the terms of the
+ * GNU General Public License (GPL).
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * WITHOUT A WARRANTY OF ANY KIND. ALL EXPRESS OR IMPLIED CONDITIONS,
+ * REPRESENTATIONS AND WARRANTIES, INCLUDING ANY IMPLIED WARRANTY OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT, ARE
+ * HEREBY EXCLUDED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.databene.jdbacl.swing;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.Map;
+
+import javax.swing.AbstractAction;
+import javax.swing.Box;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+import javax.swing.JToolBar;
+
+import org.databene.commons.ConnectFailedException;
+import org.databene.commons.IOUtil;
+import org.databene.commons.ImportFailedException;
+import org.databene.commons.OrderedMap;
+import org.databene.commons.SystemInfo;
+import org.databene.gui.os.ApplicationUtil;
+import org.databene.gui.os.JavaApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Simple GUI for database browsing.<br/><br/>
+ * Created: 07.11.2011 16:28:47
+ * @since 0.7.0
+ * @author Volker Bergmann
+ */
+@SuppressWarnings("serial")
+public class JdbaclGUI extends JFrame implements JavaApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(JdbaclGUI.class);
+	
+	private EnvironmentSelector environmentSelector;
+	private DatabasePane databasePane;
+
+	private JTextField exclusionField;
+
+	public static void main(String[] args) throws IOException {
+		ApplicationUtil.prepareNativeLAF("jdbacl");
+		JdbaclGUI appAndFrame = new JdbaclGUI();
+		ApplicationUtil.configureApplication(appAndFrame);
+		appAndFrame.setVisible(true);
+	}
+
+	public JdbaclGUI() {
+		this.exclusionField = new JTextField();
+		this.environmentSelector = new EnvironmentSelector();
+		this.databasePane = new DatabasePane(new TextFieldValueProvider(exclusionField));
+		this.environmentSelector.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent evt) {
+				String environment = environmentSelector.getSelectedItem();
+				try {
+					databasePane.setEnvironment(environment);
+				} catch (ConnectFailedException ex) {
+					LOGGER.error("Error importing environment " + environment, ex);
+				} catch (ImportFailedException ex) {
+					LOGGER.error("Error importing environment " + environment, ex);
+				}
+			}
+		});
+		createMenuBar();
+		createToolBar();
+		getContentPane().add(databasePane, BorderLayout.CENTER);
+		setSize(800, 600);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		restoreState();
+	}
+	
+	private void createToolBar() {
+		JToolBar toolBar = new JToolBar();
+		add(toolBar, BorderLayout.NORTH);
+		toolBar.add(new JLabel("Exclusion:"));
+		toolBar.add(exclusionField);
+		toolBar.add(Box.createHorizontalStrut(8));
+		toolBar.add(new JLabel("Environment:"));
+		toolBar.add(environmentSelector);
+		toolBar.add(Box.createHorizontalGlue());
+		toolBar.setFloatable(false);
+	}
+
+	private void createMenuBar() {
+	    JMenuBar menubar = new JMenuBar();
+	    
+	    // create file menu
+	    JMenu fileMenu = new JMenu("File");
+	    fileMenu.setMnemonic('F');
+	    menubar.add(fileMenu);
+	    if (!SystemInfo.isMacOsx())
+	    	fileMenu.add(new AbstractAction("Exit") {
+				public void actionPerformed(ActionEvent evt) {
+					exit();
+				}
+	    	});
+	    
+	    // create edit menu
+	    JMenu editMenu = new JMenu("Edit");
+	    editMenu.setMnemonic('E');
+	    menubar.add(editMenu);
+
+	    // create help menu
+	    JMenu helpMenu = new JMenu("Help");
+	    editMenu.setMnemonic('H');
+	    menubar.add(helpMenu);
+	    if (!SystemInfo.isMacOsx())
+	    	helpMenu.add(new AbstractAction("About") {
+				public void actionPerformed(ActionEvent evt) {
+					about();
+				}
+	    	});
+	    
+		setJMenuBar(menubar);
+    }
+
+	public void about() {
+		JOptionPane.showMessageDialog(this, 
+				"DB Sanity GUI " + SystemInfo.getLineSeparator() + // include version info
+				"(c) 2011 by Volker Bergmann");
+    }
+
+	public void exit() {
+		saveState();
+		System.exit(0);
+	}
+
+	private void saveState() {
+		try {
+			Map<String, String> props = new OrderedMap<String, String>();
+			props.put("exclusionPattern", exclusionField.getText());
+			IOUtil.writeProperties(props, "state.properties");
+		} catch (IOException e) {
+			// writing the file failed but isn't tragic
+		}
+	}
+
+	private void restoreState() {
+		try {
+			Map<String, String> props = IOUtil.readProperties("state.properties");
+			exclusionField.setText(props.get("exclusionPattern"));
+		} catch (Exception e) {
+			// no file defined yet, use default settings
+		}
+	}
+
+}
