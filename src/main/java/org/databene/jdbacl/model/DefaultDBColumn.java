136e08c5a667801b40cb6ab8f2c67d3b6363f904
vbergmann <vbergmann@ae0afc29-7cfe-489a-95cf-4bb878f6dcf4>
2012-02-02 12:46:22 +0000

    Workaround for NPE
    git-svn-id: https://svn.code.sf.net/p/jdbacl/code/trunk@489 ae0afc29-7cfe-489a-95cf-4bb878f6dcf4

--- src/main/java/org/databene/jdbacl/model/DefaultDBColumn.java
+++ src/main/java/org/databene/jdbacl/model/DefaultDBColumn.java
@@ -152,7 +152,7 @@ public class DefaultDBColumn extends AbstractDBTableComponent implements DBColum
         } else {
             // if there needs to be a NotNullConstraint, check if there exists one, first
             if (this.isNullable()) {
-				String constraintName = getTable().getName() + '_' + name + "_NOT_NULL"; // TODO v0.7 get constraint name from DB
+				String constraintName = (getTable() != null ? getTable().getName() : "_") + '_' + name + "_NOT_NULL"; // TODO v0.7 get constraint name from DB
 				this.notNullConstraint = new DBNotNullConstraint(getTable(), constraintName , true, name);
 			}
         }
